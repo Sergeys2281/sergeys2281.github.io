cb0939fd3d06419a13653867b3e8bc66ef7ae92b
Sergeys2281 <61617650+Sergeys2281@users.noreply.github.com>
2026-03-02 10:59:13 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Скрипт для мобільного меню
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
